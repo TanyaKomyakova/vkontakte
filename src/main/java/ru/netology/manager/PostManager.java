9c4c2177044d8637e9d4e179b6807fe2f14e6e88
TanyaKomyakova <tanya_komyakova@mail.ru>
2020-06-09 21:28:44 +0300

    added PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,27 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    private int ownerId;
+    private String domain;
+    private String query;
+    private boolean ownersOnly;
+    private int count;
+    private int offset;
+    private int postId;
+
+
+    public Post[] search(){
+        // TODO: add logic
+        return null;
+
+    }
+
+    public void delete(){
+        // TODO: add logic
+        return;
+    }
+}
